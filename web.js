fc7a4e935341fd49e9ba991cdcd57c9199601b21
Alexandre Assouad <alexandre.assouad@gmail.com>
2013-09-13 15:20:35 +0200

    added socket io and index file

--- web.js
+++ web.js
@@ -6,7 +6,9 @@
  * To change this template use File | Settings | File Templates.
  */
 
-var express = require("express");
+var express = require("express")
+    , io = require('socket.io').listen(app)
+    , fs = require('fs');
 
 var app = express();
 app.use(express.logger());
@@ -15,7 +17,36 @@ app.get('/', function(request, response) {
     response.send('Hello World!');
 });
 
+
+app.get('/index.html', function(request, response) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                return response.send('Error loading index.html');
+            }
+
+            response.send(data);
+        });
+});
+
+
+io.configure(function () {
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
+});
+
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
+
+
