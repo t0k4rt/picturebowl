91c15384fe680740d67dc54297fced73a12063ff
Alexandre Assouad <alexandre.assouad@gmail.com>
2014-10-24 11:09:44 +0200

    updated admin

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,12 +4,25 @@
 
 var $tag = $('#tag');
 var $msgContainer = $('#msg-container');
-$('#submit').click(function(event){
+$('#subscribe').click(function(event){
   console.log($tag.val());
   $.post('/tag', {tag: $tag.val()})
     .done(function(data) {
       console.log(data);
       $msgContainer.append('<div class="alert alert-success">Sauvegardé</div>')
+    })
+    .fail(function(data) {
+      var error = JSON.parse(data.responseText);
+      $msgContainer.append('<div class="alert alert-danger">'+error.error+'</div>')
+    });
+});
+
+$('#unsubscribe').click(function(event){
+  console.log($tag.val());
+  $.get('/unsubscribe')
+    .done(function(data) {
+      console.log(data);
+      $msgContainer.append("<div class='alert alert-success'>Le tag n'est plus surveillé</div>")
     })
     .fail(function(data) {
       var error = JSON.parse(data.responseText);
